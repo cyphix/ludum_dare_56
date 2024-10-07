11ace53dd7132c22d174af4d189e137a8ad79fd9
Skye Gibney <skyejgibney@gmail.com>
2024-10-06 19:12:28 -0700

    Restyle hunger bar and continue implementation
    * Added new sprites for bar outline, the stomach contents bar, and the
      digestion timer bar
    * Implemented digestion timer bar
    
    Checkpoint to pull from main

--- Assets/Scripts/UI/HungerBar.cs
+++ Assets/Scripts/UI/HungerBar.cs
@@ -1,9 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HungerBar : MonoBehaviour {
-    private Transform digestionTimerElement;
-    private Stomach stomachComponent;
+    public Transform digestionTimerElement;
+    public Image stomachContentsElement;
+    public Animator starvationAnimation;
     
-    public void SetDigestionTimer(float time) {
+    public void SetDigestionTimer(float currentTime, float totalTime) {
+        digestionTimerElement.localScale = new Vector3(
+            totalTime == 0 ? 0 : currentTime / totalTime,
+            1,
+            1
+        );
+    }
+
+    public void SetStomachContents(int contents) {
+        Debug.Log("Contents!!!");
+        Debug.Log(contents);
+        int max = 10;
+
+        if (contents < 1) {
+            starvationAnimation.SetBool("Starving", true);
+        }
+        else {
+            starvationAnimation.SetBool("Starving", false);
+        }
+        
+        stomachContentsElement.transform.localScale = new Vector3(
+            contents / max,
+            1,
+            1
+        );
     }
 }
