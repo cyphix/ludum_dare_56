47cfb36fac7f2f551d6f9e5989f0ffb9ff9a939b
Skye Gibney <skyejgibney@gmail.com>
2024-10-06 17:51:00 -0700

    Fix healthbar scaling and begin hungerbar
    * Assgined hungerbar script to hungerbar gameobject
    * Added and assigned new sprite to hungerbar
    * Added digestion timer visual element below bar
    * Added apple sprites to indicate how the bar works
    
    Committing to pull changes from main

--- Assets/Scripts/UI/HungerBar.cs
+++ Assets/Scripts/UI/HungerBar.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
 
-public class HungerBar : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class HungerBar : MonoBehaviour {
+    private Transform digestionTimerElement;
+    private Stomach stomachComponent;
+    
+    public void SetDigestionTimer(float time) {
     }
 }
